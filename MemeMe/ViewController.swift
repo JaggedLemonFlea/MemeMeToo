e6b49adabd366c6be6fe938c55278052cd6ad4b0
JaggedLemonFlea <31052431+JaggedLemonFlea@users.noreply.github.com>
2018-10-02 10:23:53 -0500

    Working keyboard show/hide functions proper layout and constraints

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,29 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+// MARK: Outlets >>>
+    
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var tfTop: UITextField!
+    @IBOutlet weak var tfBottom: UITextField!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var toolBar: UIToolbar!
+
+// <<< Outlets
     
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tfTop.delegate = self
+        tfBottom.delegate = self
         imagePicker.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // subscribe to keyboard notifications, to allow view to raise when keyboard is displayed
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        shareButton.isEnabled = imagePickerView.image != nil
+        cancelButton.isEnabled = imagePickerView.image != nil
         subscribeToKeyboardNotifications()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        // unsubscribe to keyboard notifications, to allow view to lower when keyboard dissapears
         unsubscribeFromKeyboardNotifications()
     }
+    
+// MARK:  Actions >>>
     
     @IBAction func pickAnImageFromAlbum(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary
@@ -41,6 +55,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
+    
+// <<< Actions
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
@@ -55,22 +71,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
-    @objc func keyboardWillShow(_ notification: Notification) {
-        view.frame.origin.y = -getKeyboardHeight(notification)
+    
+// MARK: Keyboard Functions >>>
+    
+    // Subscribe to keyboard notifications
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    // Unsubscribe to keyboard notifications
+    func unsubscribeFromKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    // Move frame up when botton text field is clicked and keyboard shows
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if tfBottom.isEditing {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    
+    // Move frame back down after keyboard is for bottom text field is dismissed
+    @objc func keyboardWillHide(_ notification: Notification){
+        view.frame.origin.y = 0
+    }
+    
+    // Get keyboard height
     func getKeyboardHeight(_ notification:Notification) -> CGFloat {
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
         return keyboardSize.cgRectValue.height
     }
     
-    func subscribeToKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    // Dismisses the keyboard when return button is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
     }
     
-    func unsubscribeFromKeyboardNotifications(){
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
+// <<< Keyboard Functions
 }
 
